849c0d5e5bd20ad08625f9803a8709a7dd443d84
Vaibhav Vijay <vaibhavvijay9@gmail.com>
2022-10-08 13:20:57 +0530

    heroku deploy

---
+++ target/classes/todoapp.sql
@@ -0,0 +1,75 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 5.0.67-community-nt : Database - todoapp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`todoapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `todoapp`;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `category_id` int(10) NOT NULL auto_increment,
+  `category_name` varchar(30) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY  (`category_id`),
+  UNIQUE KEY `category_name` (`category_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Data for the table `categories` */
+
+/*Table structure for table `sessions` */
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `username` varchar(80) NOT NULL,
+  `token` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `sessions` */
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `task_id` int(10) NOT NULL auto_increment,
+  `task_description` varchar(200) NOT NULL,
+  `task_date` date NOT NULL,
+  `is_completed` tinyint(1) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `category_id` int(10) NOT NULL,
+  PRIMARY KEY  (`task_id`),
+  KEY `FK_tasks` (`category_id`),
+  CONSTRAINT `FK_tasks` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tasks` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `username` varchar(80) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY  (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
